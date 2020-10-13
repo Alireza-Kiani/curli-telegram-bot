7df574061df6ec82c69293b0347cb7895f0caaee
Mahdi Karimi <mhdi_kr@outlook.com>
2020-10-13 15:46:24 +0330

    functional bot interface for curli

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Telegraf = require('telegraf')
+const validator = require('validator')
+const fetch = require("node-fetch");
+require('dotenv').config()
+
+const shorten = async function (inputLink) {
+    if (validator.isURL(inputLink)){
+        console.log('the reply is url')
+        const response = await fetch('https://www.dumas.ir/shortener', {
+            method: 'post',
+            body: JSON.stringify({
+                link: inputLink
+            }),
+            headers: {
+                'content-type': 'application/json'
+            },
+            redirect: 'follow'
+        })
+        const result = await response.json()
+        console.log(result.shortLink)
+        return  'here\'s your link : ' + 'https://dumas.ir/' + result.shortLink
+    } else {
+        console.log('the reply is NOT url')
+        return 'send a valid URL'
+    }
+}
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => {
+    ctx.reply('Welcome to curli, send me your links so I can shorten them and give them to you!')
+})
+bot.on('message', async (ctx) => {
+    ctx.reply(await shorten(ctx.update.message.text))
+})
+bot.launch()
+
